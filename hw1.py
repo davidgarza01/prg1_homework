92dc7ef303d84ef6c951cd3d9c21b131b4850780
davidgarza01 <43069602+davidgarza01@users.noreply.github.com>
2018-09-14 09:06:06 -0500

    Create hw1.py

---
+++ hw1.py
@@ -0,0 +1,20 @@
+"""
+problem 1
+"""
+x = 5
+y = 5.9
+z = True
+w = "62"
+"""
+problem 2 
+"""
+a = float(input("please enter a number for a"))
+A_t = ((3**1 / 2) /4) * a 
+A_s = a**2
+b = .25
+c = 5**(1/2)
+d = 5 * (5 + 2 * c)
+A_p = b * (d**(1/2)) * (a**2)
+print(" The area of the pentagon is " + str(A_p))
+print(" The area of the triangle is " + str(A_t))
+print(" The area of the square is " + str(A_s))
