f26a219a2199d13daf24524e5ea9a637aba44040
David Garza Assad <davidgarza01@tests-MacBook-Air.local>
2018-10-11 10:16:21 -0500

    complete

--- Practice_code/fact.py
+++ Practice_code/fact.py
@@ -3,26 +3,24 @@ a strong number is defined as a number thas has the sum of the factorial of each
 
 '''
 
-print("Please enter a number that might be strong")
-snumber = input(">")
+for number_to_check in range(10000001):
+    snumber = str(number_to_check)
 
 #get all of the digits (split)
-digits = list(snumber)
-sum = []
+    digits = list(snumber)
+    sum = []
 #calculate factorial 
-for digit in digits:
-    factorial = 1
-    for number in range(1,int(digit)+1):
-        factorial = number * factorial
-    print(factorial)
-    sum.append(factorial)
+    for digit in digits:
+        factorial = 1
+        for number in range(1,int(digit)+1):
+            factorial = number * factorial
+        sum.append(factorial)
 
 #add all factorial together 
-final_factorial = 0
-for number in sum:
-    final_factorial += number 
-print("the final factorial is", final_factorial)      
+    final_factorial = 0
+    for number in sum:
+        final_factorial += number    
 #check if the sum is same as the original       
-if(final_factorial == snumber):
-    print(snumber, "this number is strong")
+    if(final_factorial == int(snumber)):
+        print(snumber, "this number is strong")
 
--- hw4pt1.py
+++ hw4pt1.py
@@ -1,7 +1,9 @@
 print("Give me a number")
-number = int(input(">"))
-for x in range(1, number + 1):2
-    if number % x == 0:
-        print(x)
+number = input(">")
+factors_list =[]
+for x in range(1, int(number) + 1):
+    if (int(number) % x == 0):
+        factors_list.append(x)
+print(number,"has",len(factors_list),"factors, ","which are", factors_list)
 
     
