dbb09149534eb5f7f9bd180c938a99a93d0b8829
David Garza Assad <davidgarza01@tests-MacBook-Air.local>
2018-10-24 22:31:43 -0500

    complete

--- Practice_code/DictionaryQuiz.py
+++ Practice_code/DictionaryQuiz.py
@@ -2,5 +2,7 @@ names_nicknames = {"David" : "Güero" , "Santiago" : "Cabezon"}
 names_nicknames["Deleon"] = "Cholo"
 names_nicknames["Camacho"] = "Indio"
 names_nicknames["Moore"] = " Zombie" 
+description= ""
 for x in names_nicknames:
-    print(x, "goes by the name of", names_nicknames["David"])
+    description += x + " goes by the name " +names_nicknames[x] + ", "
+print(description)
--- hw5.py
+++ hw5.py
@@ -1,12 +1,23 @@
 import random 
-
-player1 = input("Enter your name, you are player 1 ")
-player2 = input("Enter your name, you are player 2 ")
-
-players = [player1 , player2]
-scores ={}
-print(players)
-for x in scores:
-    dice = random.randint(1,6)
-    scores["turn1player1"] = dice
-print(scores[turn1player1])
+players = {}
+players["player1"] = input("What is your name you are player 1")
+players["player2"] = input("What is your name you are player 2")
+scores = {}
+less_rounds = 0
+min_rounds = 3 
+while (less_rounds < min_rounds):
+    print("This is round number " + str(less_rounds + 1))
+    score = {}
+    for player in players.values():
+        print("It is your turn", player)
+        input("Press enter to roll")
+        roll_dice = random.randint(1,6)
+        print(roll_dice)
+        score[player] = int(roll_dice)
+    if (score[players["player1"]] < score[players["player2"]]):
+        print(players["player2"], " won this round")
+    elif (score[players["player1"]] > score[players["player2"]]):
+        print(players["player1"], " won this round")
+    elif (score[player["player1"]] == score[player["player2"]]):
+        print("It is a tie, try again")
+    less_rounds += 1
